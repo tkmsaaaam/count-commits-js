ec2042b9922cdeafb583ad27b4a380032bd35638
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-24 19:37:25 +0900

    use timezone: Asia/Tokyo

--- src/github.js
+++ src/github.js
@@ -4,16 +4,13 @@ export default async () => {
   const user = process.argv[2]
   const url = `https://api.github.com/users/${user}/events`;
   const res = await fetch(url).then(response =>  response.json());
-  const today = new Date();
+  const today = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
 
   let count = 0;
 
   res.forEach(e => {
-    const created_at = new Date(e.created_at);
-    if (created_at.getFullYear() !== today.getFullYear()) return;
-    if (created_at.getMonth() !== today.getMonth()) return;
-    if (created_at.getDate() !== today.getDate()) return;
-    if (e.type == 'PushEvent') { count += 1 }
+    const created_at = new Date(e.created_at).toLocaleString({ timeZone: 'Asia/Tokyo' });
+    if (today.slice(0,9) == created_at.slice(0,9) && e.type == 'PushEvent') { count += 1 }
   });
   return count;
 };
