8d77edcab7523b4b525ea5491578747fe8c7b554
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-03-11 18:29:52 +0900

    add main_test.go

---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestCreateMessage(t *testing.T) {
+	type args struct {
+		countCommitsToday int
+		countDays         int
+		userName          string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "notCommitedAndNoStreak",
+			args: args{countCommitsToday: 0, countDays: 0, userName: "octocat"},
+			want: "<!channel> 今日はまだコミットしていません！\n連続コミット日数は0\nhttps://github.com/octocat",
+		},
+		{
+			name: "notCommitedAnd",
+			args: args{countCommitsToday: 0, countDays: 1, userName: "octocat"},
+			want: "<!channel> 今日はまだコミットしていません！\n連続コミット日数は1\nhttps://github.com/octocat",
+		},
+		{
+			name: "commitedNoStreak",
+			args: args{countCommitsToday: 1, countDays: 0, userName: "octocat"},
+			want: "\n今日のコミット数は1\n連続コミット日数は0\nhttps://github.com/octocat",
+		},
+		{
+			name: "commited",
+			args: args{countCommitsToday: 1, countDays: 2, userName: "octocat"},
+			want: "\n今日のコミット数は1\n連続コミット日数は2\nhttps://github.com/octocat",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createMessage(tt.args.countCommitsToday, tt.args.countDays, tt.args.userName); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
