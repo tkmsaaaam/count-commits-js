9867de6522b2cc3e901915388373ba54417c27f8
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-04-23 11:04:07 +0900

    fix for-loop

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func countOverAYear(userName string, graphqlClient *githubv4.Client) (int, int) 
 	var countDays int
 	var todayContributionCount int
 	var streak int
-	for i := 0; i == 0 || streak >= 364; i++ {
+	for i := 0; i == 0 || (todayContributionCount == 0 && streak == 364) || streak > 364; i++ {
 		from := DateTime{time.Now().AddDate(-(i + 1), 0, 1)}
 		to := DateTime{time.Now().AddDate(-i, 0, 0)}
 		variables := map[string]interface{}{
